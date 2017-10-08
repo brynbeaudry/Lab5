a8c8af40c100af328fd62ee7bafa2813ee144f01
Bryn Beaudry <babeaudry@gmail.com>
2017-10-07 19:54:53 -0700

    Student Controller Made, production db seeded

--- Lab5/Models/IdentityModels.cs
+++ Lab5/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
 
 namespace Lab5.Models
 {
@@ -29,5 +30,7 @@ namespace Lab5.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Student> Students { get; set; }
     }
 }
